a4f4c6f899c78be1910c3d0033308ab8cd841d1f
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-11 09:08:21 -0400

    starting point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <div style={{ textAlign: 'center' }}>Welcome to Fitness Tracker</div>
+  return <div>Welcome to Fitness Tracker</div>
 }
 
 export default App
